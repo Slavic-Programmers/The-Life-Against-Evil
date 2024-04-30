caf6a242c34e0a095a1b3b122bbf8a739a800e44
Gavriel Linoy <kraserultra@gmail.com>
2024-04-30 21:39:54 +0300

    Continue setting up stuff

--- src/player.py
+++ src/player.py
@@ -1,5 +1,11 @@
+from settings import *
+
 class Player:
     def __init__(self) -> None:
         self.hp = 100
         self.food = 5
         self.water = 5
+        self.ammo = {
+            '9mm' : 20,
+        }
+        self.weapon = PISTOL
